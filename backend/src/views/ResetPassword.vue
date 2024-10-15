bbb1a27ac2f7b5d326abb5cc9587701ad29989e9
King <wiwichai@gmail.com>
2024-10-15 13:12:27 +0700

    layout

---
+++ backend/src/views/ResetPassword.vue
@@ -0,0 +1,44 @@
+<template>
+    <guest-layout title="Set new password">
+        <form class="space-y-6" action="#" method="POST">
+            <div>
+                <label for="new-password" class="block text-sm font-medium leading-6 text-gray-900">New Password</label>
+                <div class="mt-2">
+                    <input id="new-password" name="password" type="password" autocomplete="password" required=""
+                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+            </div>
+            <div>
+                <label for="repeat-password" class="block text-sm font-medium leading-6 text-gray-900">Repeat
+                    Password</label>
+                <div class="mt-2">
+                    <input id="repeat-password" name="password_repeat" type="password" autocomplete="password"
+                           required=""
+                           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <div class="text-sm">
+                        <router-link :to="{name: 'login'}" class="font-semibold text-indigo-600 hover:text-indigo-500">
+                            Go back to Login
+                        </router-link>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    Submit
+                </button>
+            </div>
+        </form>
+    </guest-layout>
+</template>
+
+
+<script setup>
+import GuestLayout from "../components/GuestLayout.vue";
+</script>
