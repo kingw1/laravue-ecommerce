7e2a72c1190e18e9aafd6d7c4da48fb9369b5fe8
King <wiwichai@gmail.com>
2024-10-17 23:45:57 +0700

    vuex store

---
+++ backend/src/store/actions.js
@@ -0,0 +1,23 @@
+import axiosClient from "../axios";
+
+export function getUser({ commit }) {
+    return axiosClient.get("/user").then(({ data }) => {
+        commit("setUser", data);
+        return data;
+    });
+}
+
+export function login({ commit }, data) {
+    return axiosClient.post("/login", data).then(({ data }) => {
+        commit("setUser", data.user);
+        commit("setToken", data.token);
+        return data;
+    });
+}
+
+export function logout({ commit }) {
+    return axiosClient.post("/logout").then((response) => {
+        commit("setToken", null);
+        return response;
+    });
+}
