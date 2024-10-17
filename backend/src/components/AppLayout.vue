1005bb1c2626035974b37e1dc2a91632958cfb97
King <wiwichai@gmail.com>
2024-10-17 23:46:54 +0700

    auth user

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex min-h-full bg-gray-100">
-        <Sidebar :class="{'-ml-[200px]' : !sidebarOpened}" />
+    <div v-if="currentUser.id" class="flex min-h-full bg-gray-100">
+        <Sidebar :class="{ '-ml-[200px]': !sidebarOpened }" />
         <div class="flex-1">
-            <Navbar @toggle-sidebar="toggleSidebar"/>
+            <Navbar @toggle-sidebar="toggleSidebar" />
             <main class="p-6">
                 <div class="p-4 rounded bg-white">
                     <router-view></router-view>
@@ -10,29 +10,59 @@
             </main>
         </div>
     </div>
+
+    <!-- Loading -->
+    <div v-else class="min-h-full bg-gray-200 flex justify-center items-center">
+        <div class="flex flex-col items-center">
+            <svg
+                class="animate-spin -ml-1 mr-3 h-5 w-5 text-gray-700"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                ></circle>
+                <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+            </svg>
+            <span class="mt-2">Please Wait...</span>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import {ref, onMounted, onUnmounted} from 'vue'
+import { ref, onMounted, onUnmounted, computed } from "vue";
 import Sidebar from "./Sidebar.vue";
 import Navbar from "./Navbar.vue";
+import store from "../store";
 
-const sidebarOpened = ref(true)
+const sidebarOpened = ref(true);
+const currentUser = computed(() => store.state.user.data);
 
 function toggleSidebar() {
-    sidebarOpened.value = !sidebarOpened.value
+    sidebarOpened.value = !sidebarOpened.value;
 }
 
 onMounted(() => {
-    handleSidebarOpened()
-    window.addEventListener('resize', handleSidebarOpened)
-})
+    store.dispatch("getUser");
+    handleSidebarOpened();
+    window.addEventListener("resize", handleSidebarOpened);
+});
 
 onUnmounted(() => {
-    window.removeEventListener('resize', handleSidebarOpened)
-})
+    window.removeEventListener("resize", handleSidebarOpened);
+});
 
 function handleSidebarOpened() {
-    sidebarOpened.value = window.outerWidth > 768
+    sidebarOpened.value = window.outerWidth > 768;
 }
 </script>
