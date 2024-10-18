b1ec4b5f4d3812791bfdb6fea4b58dd98404d633
King <wiwichai@gmail.com>
2024-10-18 20:04:34 +0700

    move api controller to api folder

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'admin'])
